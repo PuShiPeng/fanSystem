31ecd44e942391604e81300df539574ba7387fe2
pu <1535708687@qq.com>
2021-12-31 22:07:46 +0800

    颠倒32位无符号整数

--- src/main/java/com/pu/fansystem/utils/StringUtil.java
+++ src/main/java/com/pu/fansystem/utils/StringUtil.java
@@ -78,4 +78,35 @@ public class StringUtil {
         }
         return -1;
     }
+
+    /**
+     * 颠倒给定的 32 位无符号整数的二进制位
+     */
+    /** 方法一：逐位颠倒 **/
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32 && n != 0; ++i){
+            res |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return res;
+    }
+    /**
+     * 方法二：位运算分治
+     * &运算：单个二进制码 & 1 = 本身，M1提取奇数位。
+     * n >>> 1 & M1 ： 提取偶数位变奇数位
+     * (n & M1) << 1 ： 提取奇数位变偶数位
+     * 以此类推，最后一层只需要交换位置，不用提取
+     */
+    private static final int M1 = 0x55555555; // 01010101010101010101010101010101
+    private static final int M2 = 0x33333333; // 00110011001100110011001100110011
+    private static final int M4 = 0x0f0f0f0f; // 00001111000011110000111100001111
+    private static final int M8 = 0x00ff00ff; // 00000000111111110000000011111111
+    public int reverseBits1(int n) {
+        n = n >>> 1 & M1 | (n & M1) << 1;
+        n = n >>> 2 & M2 | (n & M2) << 2;
+        n = n >>> 4 & M4 | (n & M4) << 4;
+        n = n >>> 8 & M8 | (n & M8) << 8;
+        return n >>> 16 | n << 16;
+    }
 }
